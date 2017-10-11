d082b6f6dd1d3cb03d37236659388453e7d602b3
Azimer <Azimer@users.noreply.github.com>
2017-10-11 09:24:28 -0500

    Updated copyrights

--- AziAudio/Mupen64plusHLE/common.h
+++ AziAudio/Mupen64plusHLE/common.h
@@ -1,23 +1,32 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   Mupen64plus-rsp-hle - common.h                                        *
- *   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/           *
- *   Copyright (C) 2014 Bobby Smiles                                       *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/****************************************************************************
+*                                                                           *
+* Azimer's HLE Audio Plugin for Project64 Compatible N64 Emulators          *
+* http://www.apollo64.com/                                                  *
+* Copyright (C) 2000-2017 Azimer. All rights reserved.                      *
+*                                                                           *
+* License:                                                                  *
+*                                                                           *
+* GNU/GPLv2 http://www.gnu.org/licenses/gpl-2.0.html                        *
+*                                                                           *
+*   Mupen64plus-rsp-hle - musyx.c                                           *
+*   Mupen64Plus homepage: http://code.google.com/p/mupen64plus/             *
+*   Copyright (C) 2014 Bobby Smiles                                         *
+*                                                                           *
+*   This program is free software; you can redistribute it and/or modify    *
+*   it under the terms of the GNU General Public License as published by    *
+*   the Free Software Foundation; either version 2 of the License, or       *
+*   (at your option) any later version.                                     *
+*                                                                           *
+*   This program is distributed in the hope that it will be useful,         *
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+*   GNU General Public License for more details.                            *
+*                                                                           *
+*   You should have received a copy of the GNU General Public License       *
+*   along with this program; if not, write to the                           *
+*   Free Software Foundation, Inc.,                                         *
+*   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.            *
+*****************************************************************************/
 
 #ifndef COMMON_H
 #define COMMON_H
--- AziAudio/SoundDriverFactory.h
+++ AziAudio/SoundDriverFactory.h
@@ -1,3 +1,13 @@
+/****************************************************************************
+*                                                                           *
+* Azimer's HLE Audio Plugin for Project64 Compatible N64 Emulators          *
+* http://www.apollo64.com/                                                  *
+* Copyright (C) 2000-2017 Azimer. All rights reserved.                      *
+*                                                                           *
+* License:                                                                  *
+* GNU/GPLv2 http://www.gnu.org/licenses/gpl-2.0.html                        *
+*                                                                           *
+****************************************************************************/
 #pragma once
 #include "common.h"
 #include "SoundDriverInterface.h"
